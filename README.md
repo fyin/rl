5f7bb996d496b823e48c14980f79e0bf92c9805b
fyin <fyin2012@gmail.com>
2025-03-11 22:14:32 -0400

    Implement PPO

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Packages
 * [dqn](./dqn/README.md)
     * This package contains a practical c Deep Q-Network (DQN) implementation, training, and inference for the CartPole-v1 environment.
+* [actor_critic](./actor_critic/README.md)
+  * This package contains a practical PPO reinforcement learning implementation and training for the CartPole-v1 environment.
---
+++ actor_critic/model.py
@@ -0,0 +1,30 @@
+from torch import nn
+from torch.distributions import Categorical
+
+class Agent(nn.Module):
+    def __init__(self, obs_dim, action_dim, hidden_dim=128):
+        super().__init__()
+        self.critic = nn.Sequential(
+            nn.Linear(obs_dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, 1)
+        )
+        self.actor = nn.Sequential(
+            nn.Linear(obs_dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.Tanh(),
+            nn.Linear(hidden_dim, action_dim),
+        )
+
+    def get_value(self, x):
+        return self.critic(x)
+
+    def get_action_and_value(self, x, action=None):
+        logits = self.actor(x)
+        probs = Categorical(logits=logits)
+        if action is None:
+            action = probs.sample()
+        return action, probs.log_prob(action), probs.entropy(), self.critic(x)
