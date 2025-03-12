c1a07b8cbd7c51123e68a99c664c68e0ece77d15
fyin <fyin2012@gmail.com>
2025-03-11 22:43:39 -0400

    Update param

--- actor_critic/config.py
+++ actor_critic/config.py
@@ -6,6 +6,6 @@ def get_config():
         "lambda": 0.95,
         "epochs": 10,
         "batch_size": 128,
-        "update_nums": 200,
+        "update_nums": 1000,
         "max_terminal_steps": 1000,
     }
