3b4381d12679b776ce8524251e9f806e16b940b1
fyin <fang@MacBookPro.frontiernet.net>
2025-03-11 22:09:56 -0400

    Improve dqn package

--- dqn/visualizer.py
+++ dqn/visualizer.py
@@ -2,23 +2,22 @@ import matplotlib.pyplot as plt
 import torch
 
 """
-Visualize episode durations over training episodes. This helps analyze how well the agent is learning over time(longer durations → better policy). 
+Visualize total accumulated scores over training episodes. 
+This helps analyze how well the agent is learning over time(longer durations → better policy). 
 """
-def plot_durations(show_result=False, episode_durations=[]):
+def plot_score(episode_scores=[], title="Result", save_path=None):
     plt.figure(1)
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
-    if show_result:
-        plt.title('Result')
-    else:
-        plt.clf()
-        plt.title('Training...')
+    scores_tensor = torch.tensor(episode_scores, dtype=torch.float)
+    plt.title(title)
     plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
+    plt.ylabel('Total score')
+    plt.plot(scores_tensor.numpy())
     # Take 100 episode averages and plot them too
-    if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+    if len(scores_tensor) >= 100:
+        means = scores_tensor.unfold(0, 100, 1).mean(1).view(-1)
         means = torch.cat((torch.zeros(99), means))
         plt.plot(means.numpy())
 
     plt.pause(0.001)  # pause a bit so that plots are updated
+    plt.savefig(save_path)
+    plt.show()
